6dbad9cedac4b8e947fae2e585d19d15894249ff
alex <alexandersegerev@gmail.com>
2024-09-11 17:14:52 -0400

    Added New CSS

--- recipe-form/src/App.js
+++ recipe-form/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { RecipeProvider } from './components/RecipeContext';
 import { AddRecipe } from './components/AddRecipe';
 import { RecipeList } from './components/RecipeList';
+import "./App.css";
 
 function App() {
 	return (
 		<RecipeProvider>
 			<div>
-				<h1>My Recipes</h1>
+				<h1>Sanji's Recipes</h1>
 				<AddRecipe />
 				<RecipeList />
 			</div>
